1e8673bec6270babe706f2ff349e526004abb719
Korèn <kpapazian@edenschool.fr>
2023-03-17 16:14:41 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 let articles = document.getElementsByClassName("movie");
+let section = document.getElementById("all_movies");
 
 for (let i = 0; i < articles.length; i++) {
     articles[i].addEventListener("click", () => {
         console.log("hello");
 
-        articles[i].style.position = "fixed";
-        articles[i].style.width = "30%";
-        articles[i].style.height = "90%";
+        if (articles[i].classList.contains("movie_off")) {
+            articles[i].classList.remove("movie_off");
+            articles[i].classList.add("movie_on");
+        } else {
+            // section.addEventListener("click", () => {
+                articles[i].classList.remove("movie_on");
+                articles[i].classList.add("movie_off");
+            // });
+        }
     });
+
+
 }
